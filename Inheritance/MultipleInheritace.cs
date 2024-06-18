ad21057197e0938803a5765abef5e993527785e1
Jalindranath <jalindranathkangane38@gmail.com>
2024-06-18 10:47:35 +0530

    Multipla Inheritance

---
+++ Inheritance/MultipleInheritace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+
+
+    public interface IFlyable
+    {
+        void Fly();
+    }
+
+    public interface ISwimmable
+    {
+        void Swim();
+    }
+
+
+    public class Duck : IFlyable, ISwimmable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("The duck is flying.");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("The duck is swimming.");
+        }
+    }
+
+    internal class MultipleInheritace
+    {
+        public static void Print()
+        {
+            Duck duck = new Duck();
+            duck.Fly();
+            duck.Swim();
+
+            // Using interface references
+            IFlyable flyable = duck;
+            ISwimmable swimmable = duck;
+
+            flyable.Fly();
+            swimmable.Swim();
+        }
+    }
+
+}
+
+    
