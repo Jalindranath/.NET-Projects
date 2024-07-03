8770a7570657f845faae437426786b59f82cbd83
Jalindranath <jalindranathkangane38@gmail.com>
2024-07-03 18:59:23 +0530

    Some new HTML project added

---
+++ ASP.NET/Bulky/BulkyWeb/Migrations/20240628112051_catoagoryseeding.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class catoagoryseeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Catagories",
+                columns: new[] { "Id", "City", "Name" },
+                values: new object[] { 1, 1, "Jalindranth" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
